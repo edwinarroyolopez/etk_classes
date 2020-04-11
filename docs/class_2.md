fecb65c06e27337b1694b1da9667794870d7c1c0
Edwin Arroyo <edwin.arroyo@estratek.com.co>
2020-04-10 21:42:00 -0500

    second class

---
+++ docs/class_2.md
@@ -0,0 +1,23 @@
+## boostrap =>
+    1. construir una tabla
+    2. contruir un modal - popup
+    3. info => back
+
+    => El front diseñe componentes
+            => Como son textbox
+                        checkbox
+                        botones
+## NODE JS => back
+    => Entender como funciona
+
+        ==> Recibe peticiones de un front 
+                => Inicio de sesión 
+                    user
+                    pass
+                    ==> Click => backend ==> hace algo
+## commans npm 
+    - npm init -y: => me permite crear un archivo package.json
+    donde se almacenaran mis paquetes
+    - npm i --save express body-parser cors 
+    me permite instalar esos paquetes
+     => express ==> es un paquete que me permite levantar un servidor
---
+++ server/server.js
@@ -0,0 +1,52 @@
+/*
+    apirest
+    endpoint
+    ==>  Recibe peticiones  => front   ==> POST - GET
+    ==> Envía respuestas  => front  
+
+    BACKEND 
+            Lenguajes diferentes
+            ==> Java
+            ==> php
+            
+            ==> Node Js  ==> JavaScript  ==> Front 
+                Fácil de configurar
+                Comercial ==> fácil encontrar documentación 
+
+            ==> C# 
+            ==> Ruby
+            ==> Python
+*/
+//server
+const express = require('express')// llamando la libreria
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+const port = process.env.API_PORT || 9000
+
+// configuración del server
+app.use(cors("*"))
+app.use(bodyParser.json()) // recibir y enviar peticiones
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//endpoints => apirest
+app.get('/getUsers', async (req, res) => {
+
+    /* consulta a la db */
+    const users = [
+        { id: '1', name: 'Juan', city: 'Medellin', age:18, salary:1000000 },
+        { id: '2', name: 'Pedro', city: 'Bogotá', age:23, salary:1000000 },
+        { id: '3', name: 'Camilo', city: 'Cali', age:27, salary:1000000 },
+        { id: '4', name: 'Andres', city: 'Medellin', age:22, salary:1000000 }
+    ]
+
+    res.send({
+        users,
+    });
+
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening  at http://localhost:${port}`)
+})
